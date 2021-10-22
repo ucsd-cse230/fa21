b3c2f3e0a4a85acacae63d92b47ee6a1f7df194f
Ranjit Jhala <jhala@cs.ucsd.edu>
2021-10-22 09:24:01 -0700

    update page

--- docs/code/src/lec_10_21_21.hs
+++ docs/code/src/lec_10_21_21.hs
@@ -1,115 +1,219 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 -- TODAY
-module Lec_10_19_21 where
 
-import Text.Printf (printf)
-main :: IO ()
-main = do
-    return () -- putStrLn "Hello!" 
-    return () -- putStrLn "World!" 
+{-# LANGUAGE OverloadedStrings #-}
 
--- return :: a -> Recipe a
+module Lec_10_21_21 where
 
-app = loop 1
+import qualified Data.Text as T
+import qualified Data.Char as Char
 
-loop :: Int -> IO ()
-loop n = do {
-    putStrLn (printf "(%d) What is your name?" n); 
+data Breakfast = Egg | Smoothie
+  deriving (Eq, Ord, Show)
 
-    name <- getLine;
-    -- 
-    if null name 
-        then loop (n + 1) 
-        else putStrLn ("Hello, " ++ name)
-    -- 
-}
+data Lunch = Sandwich | Soup | Salad
+  deriving (Eq, Ord, Show)
 
-range :: Int -> Int -> [Int]
-range i j = if i <= j then i : range (i+1) j else []
+data Food = B Breakfast |  L Lunch
+  deriving (Eq, Ord, Show)
+
+-- instance Eq Lunch where
+--   (==) = eqLunch
+
+-- instance Show Lunch where
+--   show Soup = "Soup"
+--   show Sandwich = "Sand-wich"
 
 
-plus :: Num a => a -> a -> a
-plus x y = x + y
 
-leq :: Ord a => a -> a -> Bool
-leq x y = x <= y
+
+eqLunch :: Lunch -> Lunch -> Bool
+eqLunch Sandwich Sandwich = True
+eqLunch Soup     Soup = True
+eqLunch _        _  = False
 
 {- 
 
-class Monad m where
-  (>>=)  :: m a -> (a -> m b) -> m b
-  return :: a -> m a
+class Num a where 
+  (+) :: a -> a -> a
+
+class Eq a where
+  (==) :: a -> a -> Bool
+  
+class Ord a where
+  compare :: a -> a -> Ordering 
+
 -}
 
--- >>> leq ['9'.1" "10"
--- False
+-- TYPECLASSES
 
-quiz = 
-  range 1 3 >>= \i ->
-    range i 3 >>= \j -> 
-      return (i, j)
+-- FUNCTOR 
 
-foo = (+)
-
--- >>> quiz
--- [(1,1),(1,2),(1,3),(2,2),(2,3),(3,3)]
-
-{- -}
-
--- [ i * i for i in range(1, 10) ]
--- >>> [ i * i | i <- [1..10] ]
--- [1,4,9,16,25,36,49,64,81,100]
+-- MONAD
 
 
 
 
-silly :: IO ()
-silly =
-    putStrLn "What is your name?" >> 
-      getLine >>= \name -> 
-          putStrLn ("Hello, " ++ name)
+data JVal
+  = JStr  T.Text
+  | JNum  Double
+  | JBool Bool
+  | JObj  [(T.Text, JVal)]
+  | JArr  [JVal]
+  deriving (Eq, Ord, Show)
 
-drain = runEach 
-  [ putStrLn "Yello"
-  , putStrLn "Fello"
-  , putStrLn "Jello"
-  , putStrLn "Hello"
-  , putStrLn "World!" 
+
+
+
+-- >>> textJVal "ranjit"
+-- JStr "ranjit"
+
+js1 :: JVal
+js1 =
+  JObj [("name", JStr "Ranjit")
+       ,("age",  JNum 44.0)
+       ,("likes",   JArr [ JStr "guacamole", JStr "coffee", JStr "bacon"])
+       ,("hates",   JArr [ JStr "waiting"  , JStr "grapefruit"])
+       ,("lunches", JArr [ JObj [("day",  JStr "monday")
+                                ,("loc",  JStr "zanzibar")]
+                         , JObj [("day",  JStr "tuesday")
+                                ,("loc",  JStr "farmers market")]
+                         , JObj [("day",  JStr "wednesday")
+                                ,("loc",  JStr "hare krishna")]
+                         , JObj [("day",  JStr "thursday")
+                                ,("loc",  JStr "faculty club")]
+                         , JObj [("day",  JStr "friday")
+                                ,("loc",  JStr "coffee cart")]
+                         ])
+       ]
+
+data Day = Mon | Tue | Wed | Thu | Fri deriving (Eq, Ord, Show)
+
+data Eat = MkEat { day :: Day , loc :: T.Text }
+
+instance ToJVal Day where
+  jval d = JStr (T.pack (show d))
+
+instance ToJVal Eat where
+  jval (MkEat d l) = JObj [ ("day", jval d), ("loc", jval l) ]
+
+instance ToJVal T.Text where
+  jval s = JStr s
+
+instance ToJVal Int where
+  jval i = JNum (fromIntegral i)
+
+instance ToJVal a => ToJVal [a] where
+  jval xs = JArr (map jval xs)
+
+class ToJVal a where
+  jval :: a -> JVal
+
+lunches :: [Eat]
+lunches =
+  [ MkEat Mon "zanzibar"
+  , MkEat Tue "farmers market"
+  , MkEat Wed "lemongrass"
+  , MkEat Thu "art of espresso"
+  , MkEat Fri "faculty club"
   ]
- 
-blob = do {
-    putStrLn "Hello";
-    putStrLn "Hello";
-    putStrLn "Hello";
-    putStrLn "Hello";
-    putStrLn "World!" 
-  } 
-    
--- runEach [putStrLn "Hello", putStrLn "World"]
-runEach :: [IO ()] -> IO ()
-runEach = foldr (>>) (return ())
--- runEach []     = return () 
--- runEach (r:rs) = r >> runEach rs
 
 
--- runEach [r1,r2,r3,r4,r5] = r1 >> r2 >> r3 >> r4 >> r5
--- runEach [] = error "need_recipe_that_does_nothing"
+
+-- >>> jval [lunches]
+-- JArr [JArr [JObj [("day",JStr "Mon"),("loc",JStr "zanzibar")],JObj [("day",JStr "Tue"),("loc",JStr "farmers market")],JObj [("day",JStr "Wed"),("loc",JStr "lemongrass")],JObj [("day",JStr "Thu"),("loc",JStr "art of espresso")],JObj [("day",JStr "Fri"),("loc",JStr "faculty club")]]]
+
+
+data Table key val
+  = Emp
+  | Ent key val (Table key val)
+  deriving (Show, Functor)
+
+
+-- >>> jval lunches' 
+-- JObj [("Mon",JStr "zanzibar"),("Tue",JStr "farmers market"),("Wed",JStr "lemongrass"),("Thu",JStr "art of espresso"),("Fri",JStr "faculty club")]
+
+-- >>> angry "zanzibar"
+-- "ZANZIBAR"
+
+angry :: [Char] -> [Char]
+angry str = map (Char.toUpper) str
+
+-- >>> map (Char.toUpper) ['z', 'a', 'n']
+-- 
+-- >>> tableMap angry lunches' 
+
+tableMap :: (a -> b) -> Table key a -> Table key b 
+tableMap f Emp = Emp
+tableMap f (Ent k v rest) = Ent k (f v) (tableMap f rest)
+
+class Mappable thing where
+  genMap :: (a -> b) -> thing a -> thing b 
+
+instance Mappable [] where 
+  genMap = map
+
+-- Functor
+
+instance Mappable (Table k) where 
+  genMap = tableMap
+
+-- >>> fmap angry ["cat", "dog"]
+-- ["CAT","DOG"]
+-- >>> fmap (angry . T.unpack) lunches'
+-- Ent Mon "ZANZIBAR" (Ent Tue "FARMERS MARKET" (Ent Wed "LEMONGRASS" (Ent Thu "ART OF ESPRESSO" Emp)))
+
+{- 
+
+tableMap :: (a -> b) -> Table key a -> Table key b 
+map      :: (a -> b) -> List a      -> List      b
+
+map f [] = []
+map f (x:xs) = f x : map f xs
+
+-}
+
+lunches' :: Table Day T.Text
+lunches' =
+  Ent Mon "zanzibar"
+    (Ent Tue "farmers market"
+      (Ent Wed "lemongrass"
+        (Ent Thu "art of espresso"
+          Emp)))
+
+instance (Show key, ToJVal val) => ToJVal (Table key val) where
+  jval t = JObj [ (T.pack (show k), jval v) | (k, v) <- toList t]
+
+toList :: Table k v -> [(k, v)]
+toList Emp = []
+toList (Ent k v rest) = (k, v) : toList rest
+
+
+-- >>> get "hungry" Fri lunches'
+-- "hungry"
+
+get :: Eq k => v -> k -> Table k v -> v
+get def key Emp = def
+get def key (Ent k v rest) = if key == k then v else get def key rest
 
 
 {- 
-r            :: IO ()
-runEach rs   :: IO ()
-r >> runEach :: IO ()
 
-(>>) :: IO () -> IO () -> IO ()
+        Mon Tue __ Thu Fri
 
-r >> runEach rs 
-
-(>>=) :: IO a -> (a -> IO b) -> IO b
-
-
-1. putStrLn "What is your name?"    :: IO ()
-2. getLine                          :: IO String 
-3. >>= \name -> putStrLn ("Hello " ++ name)
-
-
+  Wed
 -}
+
+-- >>> set Tue "rubios" lunches'
+-- Ent Mon "zanzibar" (Ent Tue "farmers market" (Ent Wed "lemongrass" (Ent Thu "art of espresso" (Ent Fri "rubios" Emp))))
+
+set :: Ord key => key -> val -> Table key val -> Table key val
+set key val Emp            = Ent key val Emp
+set key val (Ent k v rest)
+  | k == key = Ent k val rest
+  | k < key  = Ent k v  (set key val rest)
+  | otherwise {- k > key -} = Ent key val (Ent k v rest)
+
+-----
